7d39b12d73aa994aa3cfb833fe96bd9317393215
goreleaserbot <goreleaser@carlosbecker.com>
2018-10-02 00:50:39 +0800

    Brew formula update for localroast version v0.2.0

--- Formula/localroast.rb
+++ Formula/localroast.rb
@@ -1,15 +1,15 @@
 class Localroast < Formula
-  desc "Lightweight HTTP server stubs"
+  desc "Localroast quickly stubs a HTTP server"
   homepage "https://github.com/caalberts/localroast"
-  url "https://github.com/caalberts/localroast/releases/download/v0.1.0/localroast_0.1.0_Darwin_x86_64.tar.gz"
-  version "0.1.0"
-  sha256 "8c7901f9cffbf94b5669bd3368317109e13a4d1041c2abccc6e0bc176397334d"
+  url "https://github.com/caalberts/localroast/releases/download/v0.2.0/localroast_0.2.0_Darwin_x86_64.tar.gz"
+  version "0.2.0"
+  sha256 "ad785768e33a1d8fbbcacc1ba8a57529da882db65ab0081aac50206f89ef2aae"
 
   def install
     bin.install "localroast"
   end
 
   test do
-    system "#{bin}/localroast -v"
+    system "#{bin}/localroast version"
   end
 end
