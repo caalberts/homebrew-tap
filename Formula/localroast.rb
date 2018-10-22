96a6e59fe8d6b9b22652c519813deccd95b7e8cf
goreleaserbot <goreleaser@carlosbecker.com>
2018-10-23 06:51:42 +0800

    Brew formula update for localroast version v0.2.1

--- Formula/localroast.rb
+++ Formula/localroast.rb
@@ -3,7 +3,7 @@ class Localroast < Formula
   homepage "https://github.com/caalberts/localroast"
   url "https://github.com/caalberts/localroast/releases/download/v0.2.1/localroast_0.2.1_Darwin_x86_64.tar.gz"
   version "0.2.1"
-  sha256 "3c1b9f00641d28e094672822f2732a98d7e880e7d6a5f3420411a532713dff54"
+  sha256 "60f234b87f6c6019968347cef34a12922a14d29bd38131bc040805201d5562bd"
 
   def install
     bin.install "localroast"
